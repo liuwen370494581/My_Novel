455d81a0e12346a3e659ece0497b2bd537a43130
liuwen <370494581@qq.com>
2017-08-25 10:12:24 +0800

    Init Project

---
+++ app/src/main/java/com/example/ruolan/letgo/Jsoup/Action/LoadLocalFileAction.java
@@ -0,0 +1,63 @@
+package com.example.ruolan.letgo.Jsoup.Action;
+
+import android.content.Context;
+import android.os.Environment;
+
+import com.example.ruolan.letgo.R;
+import com.example.ruolan.letgo.bean.BookModel;
+
+import java.io.File;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.internal.schedulers.IoScheduler;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by liuwen on 2017/8/24.
+ */
+public class LoadLocalFileAction {
+
+
+    public static void searchLocalFile(final Context context, final ActionCallBack callBack) {
+        Observable.create(new ObservableOnSubscribe<File>() {
+            @Override
+            public void subscribe(ObservableEmitter<File> e) throws Exception {
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    searchBook(e, new File(Environment.getExternalStorageDirectory().getAbsolutePath()));
+                }
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<File>() {
+            @Override
+            public void accept(@NonNull File list) throws Exception {
+                if (list.exists()) {
+                    callBack.ok(list);
+                } else {
+                    callBack.failed(context.getString(R.string.add_failed));
+                }
+            }
+        });
+    }
+
+    private static void searchBook(ObservableEmitter<File> e, File parentFile) {
+        if (null != parentFile && parentFile.listFiles().length > 0) {
+            File[] childFiles = parentFile.listFiles();
+            for (int i = 0; i < childFiles.length; i++) {
+                if (childFiles[i].isFile() && childFiles[i].getName().substring(childFiles[i].getName().lastIndexOf(".") + 1).equalsIgnoreCase("txt") && childFiles[i].length() > 100 * 1024) {   //100kb
+                    e.onNext(childFiles[i]);
+                    continue;
+                }
+                if (childFiles[i].isDirectory() && childFiles[i].listFiles().length > 0) {
+                    searchBook(e, childFiles[i]);
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/ruolan/letgo/fragment/DiscoveryFragment.java
+++ app/src/main/java/com/example/ruolan/letgo/fragment/DiscoveryFragment.java
@@ -1,41 +1,78 @@
 package com.example.ruolan.letgo.fragment;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ruolan.letgo.Base.BaseFragment;
+import com.example.ruolan.letgo.Base.Config;
+import com.example.ruolan.letgo.Dialog.TipandEditDialog;
 import com.example.ruolan.letgo.Jsoup.Action.ActionCallBack;
-import com.example.ruolan.letgo.Jsoup.Action.StartReadBookAction;
+import com.example.ruolan.letgo.Jsoup.Action.LoadLocalFileAction;
 import com.example.ruolan.letgo.R;
+import com.example.ruolan.letgo.Utils.PermissionCheck;
+import com.example.ruolan.letgo.Utils.ToastUtils;
 
 /**
  * Created by ruolan on 2015/11/29.
  * 发现
  */
 public class DiscoveryFragment extends BaseFragment {
+    private Button btnSmartScan;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
-        initView();
+        initView(view);
+        setListener();
         return view;
     }
 
-    private void initView() {
-        showLoadingDialog(getString(R.string.Being_loaded),true,null);
-        StartReadBookAction.searchDetailBookUi(getActivity(), "", new ActionCallBack() {
+    private void initView(View view) {
+        btnSmartScan = (Button) view.findViewById(R.id.btn_scan);
+
+    }
+
+    private void setListener() {
+        btnSmartScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //检查权限
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !PermissionCheck.checkPremission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    getActivity().requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0x11);
+                } else {
+                    LoadData();
+                    btnSmartScan.setVisibility(View.GONE);
+                }
+
+
+            }
+        });
+    }
+
+
+    private void LoadData() {
+        showLoadingDialog(getString(R.string.Being_loaded), true, null);
+        LoadLocalFileAction.searchLocalFile(getActivity(), new ActionCallBack() {
             @Override
             public void ok(Object object) {
-        hideLoadingDialog();
+                hideLoadingDialog();
+                Log.e(Config.TAG, object.toString());
             }
 
             @Override
             public void failed(Object object) {
-
+                hideLoadingDialog();
             }
         });
     }
@@ -45,4 +82,39 @@ public class DiscoveryFragment extends BaseFragment {
 
     }
 
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0x11) {
+            if (grantResults != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && PermissionCheck.checkPremission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                LoadData();
+                btnSmartScan.setVisibility(View.GONE);
+            } else {
+                if (!getActivity().shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    final TipandEditDialog dialog = new TipandEditDialog(getActivity(), "去系统设置打开SD卡读写权限?");
+                    dialog.show();
+                    dialog.setLeftText("取消");
+                    // dialog.setLeftTextColor(getResources().getColor(R.color.black));
+                    dialog.setRightText("确定");
+                    dialog.setListener(new TipandEditDialog.ITipEndEditDialogListener() {
+                        @Override
+                        public void ClickLeft() {
+                            dialog.dismiss();
+                        }
+
+                        @Override
+                        public void ClickRight() {
+                            PermissionCheck.requestPermissionSetting(getActivity());
+                        }
+                    });
+
+                } else {
+                    ToastUtils.showToast(getActivity(), "未获取SD卡读取权限");
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
 }
