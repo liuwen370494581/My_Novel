0ffad745832dd62ec450140793e2c06cfda197e5
liuwen <370494581@qq.com>
2017-09-18 13:48:35 +0800

    Init Project

--- app/src/main/java/com/example/ruolan/letgo/Activity/FullBookShowActivity.java
+++ app/src/main/java/com/example/ruolan/letgo/Activity/FullBookShowActivity.java
@@ -1,10 +1,20 @@
 package com.example.ruolan.letgo.Activity;
 
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ruolan.letgo.Base.BaseActivity;
 import com.example.ruolan.letgo.R;
+import com.example.ruolan.letgo.Utils.ToastUtils;
+import com.example.ruolan.letgo.widget.ReadBook.BookContentView;
 import com.example.ruolan.letgo.widget.ReadBook.ContentSwitchView;
+import com.monke.mprogressbar.MHorProgressBar;
+
+import me.grantland.widget.AutofitTextView;
 
 /**
  * Created by liuwen on 2017/8/10.
@@ -14,6 +24,25 @@ public class FullBookShowActivity extends BaseActivity {
     private FrameLayout flContent;
     private ContentSwitchView csvBook;
 
+
+    //主菜单
+    private FrameLayout flMenu;//点击显示
+    private View vMenuBg;//背景
+    private LinearLayout llMenuTop;//上面顶部栏
+    private LinearLayout llMenuBottom;//下面导航栏
+    private ImageButton ivReturn;//返回键
+    private ImageView ivMenuMore;//选择更多键
+    private AutofitTextView atvTitle;//章节目录
+    private TextView tvPre;//上一章
+    private TextView tvNext;//下一章
+    private MHorProgressBar hpbReadProgress;
+    private LinearLayout llCatalog;//分类
+    private LinearLayout llLight;//调光
+    private LinearLayout llFont;//字体
+    private LinearLayout llCache;//缓存
+    private LinearLayout llSetting;//设置
+
+
     @Override
     protected int setLayoutRes() {
         return R.layout.full_book_show_activity;
@@ -21,8 +50,35 @@ public class FullBookShowActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        flContent = (FrameLayout) findViewById(R.id.fl_content);
-        csvBook = (ContentSwitchView) findViewById(R.id.csv_book);
+        flContent = getView(R.id.fl_content);
+        csvBook = getView(R.id.csv_book);
+
+        llMenuTop = getView(R.id.ll_menu_top);
+        llMenuBottom = getView(R.id.ll_menu_bottom);
+        ivReturn = getView(R.id.iv_return);
+        ivMenuMore = getView(R.id.iv_more);
+        atvTitle = getView(R.id.atv_title);
+
+        tvPre = getView(R.id.tv_pre);
+        tvNext = getView(R.id.tv_next);
+        hpbReadProgress = getView(R.id.hpb_read_progress);
+        llCatalog = getView(R.id.ll_catalog);
+        llLight = getView(R.id.ll_light);
+        llFont = getView(R.id.ll_font);
+        llCache = getView(R.id.ll_cache);
+        llSetting = getView(R.id.ll_setting);
+
+        initCsvBook();
+    }
+
+    //初始化书籍资料
+    private void initCsvBook() {
+        csvBook.bookReadInit(new ContentSwitchView.OnBookReadInitListener() {
+            @Override
+            public void success() {
+
+            }
+        });
     }
 
     @Override
@@ -32,6 +88,46 @@ public class FullBookShowActivity extends BaseActivity {
 
     @Override
     protected void setListener() {
+        //返回键
+        ivReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
+        ivMenuMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastUtils.showToast(getActivityContext(), "选择了更多");
+            }
+        });
+
+        csvBook.setLoadDataListener(new ContentSwitchView.LoadDataListener() {
+            @Override
+            public void loaddata(BookContentView bookContentView, long tag, int chapterIndex, int pageIndex) {
+
+            }
+
+            @Override
+            public void updateProgress(int chapterIndex, int pageIndex) {
+
+            }
+
+            @Override
+            public String getChapterTitle(int chapterIndex) {
+                return null;
+            }
+
+            @Override
+            public void initData(int lineCount) {
+
+            }
+
+            @Override
+            public void showMenu() {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ruolan/letgo/bean/ChapterListModel.java
@@ -0,0 +1,77 @@
+package com.example.ruolan.letgo.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by liuwen on 2017/9/18.
+ */
+public class ChapterListModel implements Serializable {
+
+
+    private String url;//对应的每本书的url
+    private int duxChapterIndex;//当前章数
+    private String durChapterUrl;//当前章数对应的文章url;
+    private String durChapterName;//当前章节名称
+    private String tag;
+    private Boolean hasCache = false;
+
+    public ChapterListModel() {
+    }
+
+    public ChapterListModel(String url, int duxChapterIndex, String durChapterUrl, String durChapterName, String tag, Boolean hasCache) {
+        this.url = url;
+        this.duxChapterIndex = duxChapterIndex;
+        this.durChapterUrl = durChapterUrl;
+        this.durChapterName = durChapterName;
+        this.tag = tag;
+        this.hasCache = hasCache;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getDuxChapterIndex() {
+        return duxChapterIndex;
+    }
+
+    public void setDuxChapterIndex(int duxChapterIndex) {
+        this.duxChapterIndex = duxChapterIndex;
+    }
+
+    public String getDurChapterUrl() {
+        return durChapterUrl;
+    }
+
+    public void setDurChapterUrl(String durChapterUrl) {
+        this.durChapterUrl = durChapterUrl;
+    }
+
+    public String getDurChapterName() {
+        return durChapterName;
+    }
+
+    public void setDurChapterName(String durChapterName) {
+        this.durChapterName = durChapterName;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Boolean getHasCache() {
+        return hasCache;
+    }
+
+    public void setHasCache(Boolean hasCache) {
+        this.hasCache = hasCache;
+    }
+}
